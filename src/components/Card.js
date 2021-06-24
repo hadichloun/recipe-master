7345b0e0356ec8c8ed822c19707a443f61540b4b
hadi <hadi_960@hotmail.com>
2021-06-24 12:35:42 -0500

    adds like and unlike on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,27 +5,28 @@ import Ingredients from './Ingredients'
 const Card = ({recipe}) => {
 
     const [show, setShow] = useState(false)
-    const [heart, setHeart] = useState(0)
+    const [liked, setLiked] = useState(true)
 
     const showIngredients = () => {
           setShow(!show)
     }
 
-    const heartButton = () => {
-        // setHeart((count) => count + 1)
-        console.log((count) => count + 10)
+    const likeButton = () => {
+        setLiked((count) => count + 1)
+        // console.log((count) => count + 1)
         
   }
 
     return(
-          
+        
         <div className="">
             <div className="card">
                 <img className="card-img-top img-fluid" src={recipe.image} alt="Card"/>
                 <h2 className="card-text">{recipe.label.toUpperCase()}</h2>
                 <button onClick={() => showIngredients()}>Show ingredients</button>
                 {show &&  <Ingredients ingredients={recipe.ingredients}/>}
-                <button className="like-btn" onClick={() => heartButton()}> 💗</button>
+                <button className="like-btn" onClick={() => setLiked(!liked)}>
+                    { liked ? '♡' : '💗' } </button>
             </div>
         </div>
     )
